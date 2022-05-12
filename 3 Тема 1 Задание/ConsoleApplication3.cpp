7b8c1c8f31d7c80fe6f01c1e16e2cec2b34657ec
NeklyudovMaksim <105452313+NeklyudovMaksim@users.noreply.github.com>
2022-05-12 23:43:57 +0500

    Add files via upload

---
+++ 3 Тема 1 Задание/ConsoleApplication3.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <clocale>
+using namespace std;
+int main() {
+	int symbn;
+	int cnt = 0;
+	int cnt2 = 0;
+	char sym;
+	setlocale(LC_ALL, "Russian_Russia.1251");
+	cout << "Введите кол-во символов";
+	cin >> symbn;
+	cout << "Введите символ";
+	cin >> sym;
+	while (symbn > 1) {
+		symbn -= 1;
+		if (sym == ',') {
+			cnt += 1;
+		}
+		cout << "Введите символ";
+		cin >> sym;
+		if (sym == '-') {
+			cnt += 1;
+		}
+		if (cnt == 2) {
+			cnt2 += 1;
+			cnt = 0;
+		}
+		cnt = 0;
+	}
+	cout << cnt2 << " членов последовательности, где идет сначала , а потом -" << endl;
+}
