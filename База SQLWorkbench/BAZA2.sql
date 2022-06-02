6fd6479cc855f943c205506e3d3fd642c61726c0
NeklyudovMaksim <105452313+NeklyudovMaksim@users.noreply.github.com>
2022-06-02 22:20:07 +0500

    Add files via upload

---
+++ База SQLWorkbench/BAZA2.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM Teams
+LIMIT 0, 1000
+
+-- Date: 2022-06-02 22:19
+*/
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (1,'Avangard','Ilya_Kovalchuk');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (2,'Avangard','Nail Yakupov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (3,'Avangard','Peter Ceglarik');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (4,'Avangard','Shimon Grubec');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (5,'Avangard','Arseniy Gricuk');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (6,'Avangard','Aleksey Emelin');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (7,'Salavat Yulaev','Rodion Amirov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (8,'Salavat Yulaev','Viktor Tihonov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (9,'Salavat Yulaev','Grigoriy Panin');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (10,'Salavat Yulaev','Linus Umark');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (11,'Salavat Yulaev','Ravil Usmanov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (12,'CSKA','Kirill Kaprizov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (13,'CSKA','Ivan Fedotov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (14,'CSKA','Sergey Andronov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (15,'CSKA','Nikita Nesterov');
+INSERT INTO `` (`id`,`TeamName`,`TeamPlayer`) VALUES (16,'CSKA','Ilya Sorokin');
